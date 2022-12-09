be438521aa2734949c70b7e1c99561019198dfd3
hasindu sithmin <hasindusithmin64@gmail.com>
2022-12-09 08:01:23 +0000

    Create components/Header.js

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+    return (
+        <header className="w3-container w3-center w3-padding-32">
+            <h1><b>SINHALA LYRICS FORUM</b></h1>
+            <p>Let's discuss the true meaning of a song</p>
+        </header>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,15 @@
 import "../styles/w3.css"
 import "../styles/railway.css"
 import "../styles/globals.css"
+import Header from "../components/Header"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="w3-content w3-light-grey" style={{maxWidth:'1400px'}}>
+    <div className="w3-content w3-light-grey" style={{ maxWidth: '1400px' }}>
+      <>
+        <Header />
         <Component  {...pageProps} />
+      </>
     </div>
   )
 }
