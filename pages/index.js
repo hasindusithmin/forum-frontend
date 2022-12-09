4d68feab37ead97bdda66084376e3cdedcbe7735
hasindu sithmin <hasindusithmin64@gmail.com>
2022-12-09 08:12:14 +0000

    Update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@
 export default function Home(){
 
     return (
-      <div>
-        Hello World
+      <div className="w3-row">
+
+          <div className="w3-col l8 s12"></div>
+
+          <div className="w3-col l4"></div>
       </div>
     )
 
