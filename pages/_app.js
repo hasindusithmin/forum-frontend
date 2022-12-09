91b29778fac6b88e00270faf74d4380405cdcd65
hasindu sithmin <hasindusithmin64@gmail.com>
2022-12-09 07:50:00 +0000

    Update pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,11 @@ import "../styles/railway.css"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component  {...pageProps} />
+  return (
+    <div className="w3-content w3-light-grey" style={{maxWidth:'1400px'}}>
+        <Component  {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
